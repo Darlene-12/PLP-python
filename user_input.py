e2a2f66b67786b9e180aad078fb4632fd48031c3
Darlene <98302606+Darlene-12@users.noreply.github.com>
2024-11-10 16:00:40 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# A personalized program that asks users for their name,age and location and then prints out a personalized message
+name = input("What is your name?")
+Age = input ("What is your age?")
+Location = input ("Where are you located?")
+
+print(f"Hello, {name}! Your age is {Age} and you are located in {Location}.")
